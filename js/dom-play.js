3c135675275bb21efa0fc066cdfd180a6f36876b
PualaniL <130610369+PualaniL@users.noreply.github.com>
2023-07-10 17:50:59 -0700

    made changes to index and main

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,56 +1,53 @@
-//alert("Hi from DOM Play");
+//alert("Hi from DOM Play!");
+
 
 /*
 
-With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    Create an app that on click actors lines are highlighted
 
-Un-highlight (normal or otherwise) all lines from all other actors in the play
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
 
- must accommodate any number of actors, and any number of additional lines added to the page
-
- we can change the html as needs
+    The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+ 
+    HTML "hooks" may be added to the page for proper operation
 
 */
 
 //document.querySelector(".hamlet").style.backgroundColor = "yellow";
 
-
-function highlight(el){
-
-    if(el.style.backgroundColor == 'white'){//change to blue
-        el.style.backgroundColor='blue';
-    }else{//change to white 
-        el.style.backgroundColor='white';
-    } 
-
- 
+function reverseHighlight() {
+    const highlightedLines = document.querySelectorAll("[style='background-color: orange;']");
+    highlightedLines.forEach(line => {
+        line.style.backgroundColor = 'white';
+    });
 }
-function myAlert(){
-    alert("I'm clicked!");
-}
- 
-function highlightAlert(actor){
-    alert("The current actor is: " + actor);
+
+function highlightActor(actor){
+    //alert("Hi, I'm " + actor);
     for(const mySpan of spans){
         if(actor == mySpan.dataset.actor){//current actor
             mySpan.style.backgroundColor='blue';
-        }else{//other actor 
+        }else{//other actor
             mySpan.style.backgroundColor='white';
         }
     }
-
 }
 
 const spans = document.querySelectorAll("#play span");
-
 console.log(spans);
 
 for(const mySpan of spans){
-    //mySpan.addEventListener("click",myAlert); 
-
+    //mySpan.addEventListener("click", myAlert);
     mySpan.addEventListener("click",function(ev){
         highlightActor(mySpan.dataset.actor);
     });
-
-
+    //alert(mySpan.dataset.actor);
 }
+
+document.addEventListener("click", function(ev) {
+    const target = ev.target;
+    if (!target.matches("#play span")) {
+        reverseHighlight();
+    }
+});
