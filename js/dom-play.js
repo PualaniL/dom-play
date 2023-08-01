bb6c59c11df41afb20e6775af427e2910257bfaf
PualaniL <130610369+PualaniL@users.noreply.github.com>
2023-08-01 00:41:25 -0700

    made changes to dom-play.js

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,6 +1,5 @@
 //alert("Hi from DOM Play!");
 
-
 /*
 
     Create an app that on click actors lines are highlighted
@@ -15,62 +14,76 @@
 */
 
 //document.querySelector(".hamlet").style.backgroundColor = "yellow";
-function highlight(el){
-
-    if(el.style.backgroundColor == 'white'){//orange
-        el.style.backgroundColor='orange';
-    }else{//white
-        el.style.backgroundColor='white';
+function highlight(el) {
+    if (el.style.backgroundColor == "white") {
+      //orange
+      el.style.backgroundColor = "orange";
+    } else {
+      //white
+      el.style.backgroundColor = "white";
     }
-}
-/*
-function myAlert(){
-    alert("I'm clicked!");
-}
-*/
-
-function reverseHighlight() {
-    const highlightedLines = document.querySelectorAll("[style='background-color: orange;']");
-    highlightedLines.forEach(line => {
-        line.style.backgroundColor = 'white';
+  }
+  /*
+  function myAlert(){
+      alert("I'm clicked!");
+  }
+  */
+  
+  function reverseHighlight() {
+    const highlightedLines = document.querySelectorAll(
+      "[style='background-color: orange;']"
+    );
+    highlightedLines.forEach((line) => {
+      line.style.backgroundColor = "white";
     });
-}
-
-function highlightActor(actor){
-    //alert("Hi, I'm " + actor);
-    for(const mySpan of spans){
-        if(actor == mySpan.dataset.actor){//current actor
-            mySpan.style.backgroundColor='orange';
-        }else{//other actor
-            mySpan.style.backgroundColor='white';
-        }
+  }
+  
+  function highlightActor(mySpan) {
+    if (mySpan.querySelector("span[data-actor]") !== null) {
+      //current actor
+      mySpan.style.backgroundColor = "orange";
+    } else {
+      //other actor
+      mySpan.style.backgroundColor = "pink";
     }
-}
-
-const spans = document.querySelectorAll("#play span");
-console.log(spans);
-
-// for(const mySpan of spans){
-//     //mySpan.addEventListener("click", myAlert);
-//     mySpan.addEventListener("click",function(ev){
-//         highlightActor(mySpan.dataset.actor);
-//     });
-//     //alert(mySpan.dataset.actor);
-// }
-
-// document.addEventListener("click", function(ev) {
-//     const target = ev.target;
-//     if (!target.matches("#play span")) {
-//         reverseHighlight();
-//     }
-// });
-
-for(const mySpan of spans){
-    //mySpan.addEventListener("click", myAlert);
-    mySpan.addEventListener("click",function(ev){
+  }
+  
+  const spans = document.querySelectorAll("p");
+  console.log(spans);
+  
+  // for(const mySpan of spans){
+  //     //mySpan.addEventListener("click", myAlert);
+  //     mySpan.addEventListener("click",function(ev){
+  //         highlightActor(mySpan.dataset.actor);
+  //     });
+  //     //alert(mySpan.dataset.actor);
+  // }
+  
+  // document.addEventListener("click", function(ev) {
+  //     const target = ev.target;
+  //     if (!target.matches("#play span")) {
+  //         reverseHighlight();
+  //     }
+  // });
+  
+  //
+  var clickCount = 0;
+  
+  for (const mySpan of spans) {
+    mySpan.addEventListener("click", function () {
+      clickCount++;
+  
+      if (clickCount == 1) {
+        clickTimer = setTimeout(() => {
+          //alert("One click");
+          highlightActor(mySpan);
+        }, 100);
+      } else if (clickCount == 2) {
+        //alert("Double click");
+        mySpan.style.backgroundColor = "white";
         clearTimeout(clickTimer);
-            clickTimer = setTimeout(() => {
-                highlightActor(mySpan.dataset.actor);
-            }, 150); 
-        });     //alert(mySpan.dataset.actor); 
-    }
+        clickCount = 0;
+      }
+    });
+  } //alert(mySpan.dataset.actor);
+  
